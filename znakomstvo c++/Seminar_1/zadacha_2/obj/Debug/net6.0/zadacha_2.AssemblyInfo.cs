8e5d2919d3c3b5bc9d8579635a78908169ccb388
Valerii Prelovskii <Valeriy.Prelovskiy@gmail_point_com>
2022-08-21 16:02:16 +0300

    ready Seminar_1 zadacha_2

--- znakomstvo c++/Seminar_1/zadacha_2/obj/Debug/net6.0/zadacha_2.AssemblyInfo.cs
+++ znakomstvo c++/Seminar_1/zadacha_2/obj/Debug/net6.0/zadacha_2.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Seminar_1")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("zadacha_2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Seminar_1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Seminar_1")]
+[assembly: System.Reflection.AssemblyProductAttribute("zadacha_2")]
+[assembly: System.Reflection.AssemblyTitleAttribute("zadacha_2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
