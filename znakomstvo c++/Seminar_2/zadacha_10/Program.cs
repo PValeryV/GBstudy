59ba95e60bca74ebe6f19538830c607d08bb2edd
Valerii Prelovskii <Valeriy.Prelovskiy@gmail_point_com>
2022-08-24 00:13:20 +0300

    переделал 10 задачу, и выполнил 13 задачу семинар 2

--- znakomstvo c++/Seminar_2/zadacha_10/Program.cs
+++ znakomstvo c++/Seminar_2/zadacha_10/Program.cs
@@ -1,12 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Введите трехзначное число");
-int a = Convert.ToInt32(Console.ReadLine());
-
-int n= 100;
-int[] arr = new int[2];
-for (int i=0;i<2;i++,n/=10){
+void primer(int i,int n,int* arr){
     arr[i]=a/n;
     a=a%n;
+    ++i;
+    n/=10;
+    if (i<2) primer(i,n,arr);
 }
+
+
+Console.WriteLine("Введите трехзначное число");
+int a = Convert.ToInt32(Console.ReadLine());
+int i=0;
+int n= 100;
+int[] arr = new int[2];
+
+primer(i,n,arr);
 
 Console.WriteLine(arr[1]);
